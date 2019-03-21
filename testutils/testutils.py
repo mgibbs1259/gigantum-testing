3424ae4a48096cd773d5eee82bde65a481de3bfd
Mary Gibbs <mgibbs1259@gmail.com>
2019-03-21 13:53:45 -0400

    Issue #2 - Invalid Custom Docker Instruction (#11)

--- testutils/testutils.py
+++ testutils/testutils.py
@@ -13,9 +13,11 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def load_chrome_driver():
     """ Return Chrome webdriver """
     return webdriver.Chrome()
+
 
 def load_chrome_driver_headless():
     """ Return headless Chrome webdriver """
@@ -23,17 +25,21 @@ def load_chrome_driver_headless():
     options.add_argument("--headless")
     return webdriver.Chrome(options=options)
 
+
 def load_firefox_driver():
     """ Return Firefox webdriver """
     return webdriver.Firefox()
+
 
 def unique_project_name(prefix: str = "selenium-project"):
     """ Return a universally-unique project name """
     return f'{prefix}-{uuid.uuid4().hex[:8]}'
 
+
 def unique_project_description():
     """ Return a universally-unique project description """
     return ''.join([str(uuid.uuid4())[:6] for num in range(30)])
+
 
 def load_credentials(path: str = 'credentials.txt'):
     """ Return tuple of username and password """
@@ -44,9 +50,16 @@ def load_credentials(path: str = 'credentials.txt'):
     # return username (first line) and password (second line)
     return lines[0], lines[1]
 
-def custom_docker_instructions():
-    """ Return a custom Docker instruction"""
+
+def valid_custom_docker_instruction():
+    """ Return a valid custom Docker instruction"""
     return "RUN cd /tmp && git clone https://github.com/gigantum/confhttpproxy && cd /tmp/confhttpproxy && pip install -e."
+
+
+def invalid_custom_docker_instruction():
+    """ Return an invalid custom Docker instruction"""
+    return "RUN /bin/false"
+
 
 def is_container_stopped(driver):
     """ Check if the container is stopped """

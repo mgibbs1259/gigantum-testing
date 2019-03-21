1250a22f61fd566e5debb60c731bcec5085f5f69
Mary <mgibbs1259@gmail.com>
2019-03-21 14:57:24 -0400

    removed JupyterLab section from test_featured_public_projects

--- gigantum_tests/test_featured_public_projects.py
+++ gigantum_tests/test_featured_public_projects.py
@@ -7,7 +7,6 @@ import selenium
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 
 # Local packages
 import testutils
@@ -26,6 +25,7 @@ def test_featured_public_projects(driver: selenium.webdriver, *args, **kwargs):
     testutils.remove_guide(driver)
     time.sleep(2)
     # Import featured public projects
+    # TO DO - add in https://gigantum.com/gigantum-examples/allen-sdk-examples
     featured_public_projects = ["gigantum.com/meg297/military-expenditure-gdp-population",
                                 "gigantum.com/billvb/fsw-telecoms-study",
                                 "gigantum.com/randal/baltimore-sun-data-bridge-data"]
@@ -41,18 +41,5 @@ def test_featured_public_projects(driver: selenium.webdriver, *args, **kwargs):
         wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".flex>.Stopped")))
         assert driver.find_element_by_css_selector(".flex>.Stopped").is_displayed(), "Expected stopped container status"
         logging.info(f"Featured public project {project} was imported successfully")
-        # Check that featured public project can be opened in JupyterLab
-        driver.find_element_by_css_selector(".flex>.Stopped").click()
-        time.sleep(10)
-        driver.find_element_by_css_selector(".Btn--text").click()
-        time.sleep(20)
-        window_handles = driver.window_handles
-        driver.switch_to.window(window_handles[1])
-        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[title = code]")))
-        assert driver.find_element_by_css_selector("[title = code]").is_displayed(), "Expected JupyterLab to open to code"
-        logging.info("Opened JupyterLab to code successfully")
-        # Switch back to project page
-        driver.switch_to.window(window_handles[0])
-        time.sleep(10)
         driver.find_element_by_css_selector(".SideBar__nav-item--labbooks").click()
         time.sleep(2)

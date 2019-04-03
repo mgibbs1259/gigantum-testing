99555d177fc71508267008770d6b16579e4ba880
Mary <mgibbs1259@gmail.com>
2019-04-03 11:16:12 -0400

    factored out css selectors

--- gigantum_tests/test_featured_public_projects.py
+++ gigantum_tests/test_featured_public_projects.py
@@ -25,20 +25,23 @@ def test_featured_public_projects(driver: selenium.webdriver, *args, **kwargs):
     testutils.remove_guide(driver)
     time.sleep(2)
     # Import featured public projects
-    # TO DO - add in gigantum.com/randal/baltimore-sun-data-bridge-data, https://gigantum.com/gigantum-examples/allen-sdk-examples
+    import_project_elts = testutils.ImportProjectElements(driver)
+    side_bar_elts = testutils.SideBarElements(driver)
+    # TO DO - add in https://gigantum.com/gigantum-examples/allen-sdk-examples
+    # TO DO - add in gigantum.com/randal/baltimore-sun-data-bridge-data
     featured_public_projects = ["gigantum.com/meg297/military-expenditure-gdp-population",
                                 "gigantum.com/billvb/fsw-telecoms-study"]
     for project in featured_public_projects:
         logging.info(f"Importing featured public project: {project}")
-        driver.find_element_by_css_selector(".btn--import ~ .btn--import").click()
+        import_project_elts.import_existing_button.click()
         time.sleep(2)
-        driver.find_element_by_css_selector(".Import__input").send_keys(project)
+        import_project_elts.project_url_input.send_keys(project)
         time.sleep(2)
-        driver.find_element_by_css_selector("button ~ button").click()
+        import_project_elts.import_button.click()
         wait = selenium.webdriver.support.ui.WebDriverWait(driver, 200)
         wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#overview")))
         wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".flex>.Stopped")))
         assert driver.find_element_by_css_selector(".flex>.Stopped").is_displayed(), "Expected stopped container status"
         logging.info(f"Featured public project {project} was imported successfully")
-        driver.find_element_by_css_selector(".SideBar__nav-item--labbooks").click()
+        side_bar_elts.side_bar_projects_icon.click()
         time.sleep(2)

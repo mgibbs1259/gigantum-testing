9a6709e2b52068132605b70edaecc2e7102ef1f7
Mary <mgibbs1259@gmail.com>
2019-04-04 14:33:11 -0400

    messaging fixes

--- gigantum_tests/test_cloud_projects.py
+++ gigantum_tests/test_cloud_projects.py
@@ -35,7 +35,7 @@ def test_publish_sync_delete_project(driver: selenium.webdriver, *args, **kwargs
     wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".flex>.Stopped")))
 
     # Publish project
-    logging.info("Publishing private project")
+    logging.info(f"Publishing private {project_title}")
     publish_elts = testutils.PublishProjectElements(driver)
     publish_elts.publish_project_button.click()
     publish_elts.publish_confirm_button.click()
@@ -50,7 +50,7 @@ def test_publish_sync_delete_project(driver: selenium.webdriver, *args, **kwargs
     cloud_tab_first_project_title_publish = driver.find_element_by_css_selector(
         ".RemoteLabbooks__panel-title:first-child span span").text
     assert cloud_tab_first_project_title_publish == project_title, \
-        "Expected project to be the first project in the cloud tab"
+        f"Expected {project_title} to be the first project in the cloud tab"
 
     project_path = os.path.join(os.environ['GIGANTUM_HOME'], username, username,
                                 'labbooks', project_title)
@@ -58,7 +58,7 @@ def test_publish_sync_delete_project(driver: selenium.webdriver, *args, **kwargs
                                      cwd=project_path, stdout=PIPE, stderr=PIPE)
     pub_stdout = git_get_remote_command_1.stdout.readline().decode('utf-8').strip()
 
-    assert "https://" in pub_stdout, f"Expected to see a remote set for project, but got {pub_stdout}"
+    assert "https://" in pub_stdout, f"Expected to see a remote set for {project_title}, but got {pub_stdout}"
 
     publish_elts.local_tab.click()
     driver.find_element_by_css_selector(f"a[href='/projects/{username}/{project_title}']").click()
@@ -97,7 +97,7 @@ def test_publish_sync_delete_project(driver: selenium.webdriver, *args, **kwargs
     cloud_tab_first_project_title_delete = driver.find_element_by_css_selector(
         ".RemoteLabbooks__panel-title:first-child span span").text
     assert cloud_tab_first_project_title_delete != project_title, \
-        "Expected project to not be the first project in the cloud tab"
+        f"Expected {project_title} to not be the first project in the cloud tab"
 
     publish_elts.local_tab.click()
     time.sleep(2)
@@ -107,13 +107,13 @@ def test_publish_sync_delete_project(driver: selenium.webdriver, *args, **kwargs
     local_tab_first_project_title = driver.find_element_by_css_selector(
         ".LocalLabbooks__panel-title:first-child span span").text
     assert local_tab_first_project_title == project_title, \
-        "Expected project to be the first project in the local tab"
+        f"Expected {project_title} to be the first project in the local tab"
 
     git_get_remote_command_2 = Popen(['git', 'remote', 'get-url', 'origin'],
                                      cwd=project_path, stdout=PIPE, stderr=PIPE)
     del_stderr = git_get_remote_command_2.stderr.readline().decode('utf-8').strip()
 
-    assert "fatal" in del_stderr, f"Expected to not see a remote set for project, but got {del_stderr}"
+    assert "fatal" in del_stderr, f"Expected to not see a remote set for {project_title}, but got {del_stderr}"
 
 
 def test_publish_collaborator(driver: selenium.webdriver, *args, ** kwargs):
@@ -143,7 +143,7 @@ def test_publish_collaborator(driver: selenium.webdriver, *args, ** kwargs):
     wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".flex>.Stopped")))
 
     # Add collaborator
-    logging.info("Adding a collaborator to private project")
+    logging.info(f"Adding a collaborator to private {project_title}")
     publish_elts.collaborators_button.click()
     time.sleep(2)
     username2 = testutils.load_credentials(user_index=1)
@@ -169,7 +169,8 @@ def test_publish_collaborator(driver: selenium.webdriver, *args, ** kwargs):
     # Test that shared cloud project is in cloud tab
     cloud_tab_first_project_title_delete = driver.find_element_by_css_selector(
         ".RemoteLabbooks__panel-title:first-child span span").text
-    assert cloud_tab_first_project_title_delete == project_title, "Expected shared cloud project in cloud tab"
+    assert cloud_tab_first_project_title_delete == project_title, \
+        f"Expected shared cloud {project_title} in cloud tab"
 
     publish_elts.download_cloud_project_button.click()
     time.sleep(2)
@@ -177,7 +178,8 @@ def test_publish_collaborator(driver: selenium.webdriver, *args, ** kwargs):
 
     # Test that after import, the shared project opens to overview page
     shared_project_title = driver.find_element_by_css_selector(".TitleSection__namespace-title").text
-    assert project_title in shared_project_title, "After import, expected shared project to open to overview page"
+    assert project_title in shared_project_title, \
+        f"After import, expected shared {project_title} to open to overview page"
 
     testutils.log_out(driver)
 
@@ -189,7 +191,7 @@ def test_publish_collaborator(driver: selenium.webdriver, *args, ** kwargs):
     except:
         pass
     time.sleep(2)
-    logging.info("Owner deleting shared project from cloud")
+    logging.info(f"Owner deleting shared {project_title} from cloud")
     publish_elts.cloud_tab.click()
     time.sleep(2)
     wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".RemoteLabbooks__panel-title")))
@@ -205,7 +207,7 @@ def test_publish_collaborator(driver: selenium.webdriver, *args, ** kwargs):
                                      cwd=project_path, stdout=PIPE, stderr=PIPE)
     del_stderr = git_get_remote_command_1.stderr.readline().decode('utf-8').strip()
 
-    assert "fatal" in del_stderr, f"Expected to not see a remote set for project, but got {del_stderr}"
+    assert "fatal" in del_stderr, f"Expected to not see a remote set for {project_title}, but got {del_stderr}"
 
 
 

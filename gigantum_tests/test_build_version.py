76d080211954fa7b5ffd193f1fed87e59a4b09f5
Mary <mgibbs1259@gmail.com>
2019-04-24 14:47:16 -0400

    minor fix

--- gigantum_tests/test_build_version.py
+++ gigantum_tests/test_build_version.py
@@ -4,13 +4,11 @@ import requests
 import sys
 import os
 import json
+import time
 
 # Library imports
 import selenium
 from selenium.webdriver.common.by import By
-
-
-logging.basicConfig(level=logging.INFO)
 
 
 def test_edge_build_versions(driver: selenium.webdriver, *args, **kwargs):
@@ -21,15 +19,22 @@ def test_edge_build_versions(driver: selenium.webdriver, *args, **kwargs):
         driver
     """
     host = f"{os.environ['GIGANTUM_HOST']}/api/ping"
-    # get requests edge build version
+    # Get requests edge build version
+    logging.info("Getting requests edge build version")
     r = requests.get(host)
     if r.status_code != 200:
         logging.error(f"Gigantum is not found at {host}")
         sys.exit(1)
     requests_edge_build_version = json.loads(r.text)
-    # get selenium edge build version
+    # Get selenium edge build version
+    logging.info("Getting selenium edge build version")
     driver.get(host)
-    selenium_edge_build_version = json.loads(driver.find_element_by_css_selector("pre").text)
-    # assert edge build versions match
-    assert requests_edge_build_version == selenium_edge_build_version, \
-        "requests edge build version does not match selenium edge build version"
+    time.sleep(2)
+    if "chrome" == driver.name:
+        selenium_edge_build_version = json.loads(driver.find_element_by_css_selector("pre").text)
+    else:
+        driver.find_element_by_css_selector("#rawdata-tab").click()
+        selenium_edge_build_version = json.loads(driver.find_element_by_css_selector("pre").text)
+
+    assert requests_edge_build_version == selenium_edge_build_version, "requests edge build version does not match " \
+                                                                       "selenium edge build version"

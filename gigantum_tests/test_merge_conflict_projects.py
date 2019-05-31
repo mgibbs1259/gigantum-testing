4a30bba14a622657163c6d423d86955233db71a6
Mary <mgibbs1259@gmail.com>
2019-05-31 15:07:18 -0400

    abort test

--- gigantum_tests/test_merge_conflict_projects.py
+++ gigantum_tests/test_merge_conflict_projects.py
@@ -1,6 +1,7 @@
 import logging
 import time
 import os
+from subprocess import Popen, PIPE
 
 import selenium
 from selenium.webdriver.common.by import By
@@ -75,7 +76,8 @@ def test_use_mine_merge_conflict_project(driver: selenium.webdriver, *args, **kw
     # Owner resolves the merge conflict with 'Use Mine'
     cloud_project_elts = testutils.CloudProjectElements(driver)
     cloud_project_elts.merge_conflict_use_mine_button.wait(90).click()
-    cloud_project_elts.sync_cloud_project_message.wait()
+    while cloud_project_elts.sync_cloud_project_message.find().text != "Sync complete":
+        cloud_project_elts.sync_cloud_project_message.wait(3)
     # Check that merge conflict resolves to 'Use Mine'
     file_path = os.path.join(os.environ['GIGANTUM_HOME'], username, username, 'labbooks',
                              project_title, 'input', 'sample-upload.txt')
@@ -96,7 +98,8 @@ def test_use_theirs_merge_conflict_project(driver: selenium.webdriver, *args, **
     # Owner uploads file, syncs, and resolves the merge conflict with 'Use Theirs'
     cloud_project_elts = testutils.CloudProjectElements(driver)
     cloud_project_elts.merge_conflict_use_theirs_button.wait(90).click()
-    cloud_project_elts.sync_cloud_project_message.wait()
+    while cloud_project_elts.sync_cloud_project_message.find().text != "Sync complete":
+        cloud_project_elts.sync_cloud_project_message.wait(3)
     # Check that merge conflict resolves to 'Use Theirs'
     file_path = os.path.join(os.environ['GIGANTUM_HOME'], username, username, 'labbooks',
                              project_title, 'input', 'sample-upload.txt')
@@ -104,8 +107,33 @@ def test_use_theirs_merge_conflict_project(driver: selenium.webdriver, *args, **
         resolve_merge_conflict_file = resolve_merge_conflict_file.read()
 
     assert resolve_merge_conflict_file == "Collaborator", \
-        f"Merge did not resolve to 'Use Mine' expected to see 'Collaborator' in file, " \
+        f"Merge did not resolve to 'Use Theirs' expected to see 'Collaborator' in file, " \
         f"but instead got {resolve_merge_conflict_file}"
 
+
+def test_abort_merge_conflict_project(driver: selenium.webdriver, *args, **kwargs):
+    """
+    Test a merge conflict in a project in which the owner resolves it with 'Abort.'
+    """
+    # Prepare merge conflict
+    username, project_title, collaborator = prep_merge_conflict(driver)
+    # Owner uploads file, syncs, and resolves the merge conflict with 'Abort'
+    cloud_project_elts = testutils.CloudProjectElements(driver)
+    project_path = os.path.join(os.environ['GIGANTUM_HOME'], username, username,
+                                'labbooks', project_title)
+    git_get_log_command_1 = Popen(['git', 'log', '--pretty=format%H'],
+                                  cwd=project_path, stdout=PIPE, stderr=PIPE)
+    before_merge_conflict_resolve_stdout = git_get_log_command_1.stdout.readline().decode('utf-8').strip()
+    print('b:', before_merge_conflict_resolve_stdout)
+    cloud_project_elts.merge_conflict_abort_button.wait(90).click()
+    # Check that merge conflict resolves to 'Abort'
+    git_get_log_command_2 = Popen(['git', 'log', '--pretty=format%H'],
+                                  cwd=project_path, stdout=PIPE, stderr=PIPE)
+    after_merge_conflict_resolve_stdout = git_get_log_command_2.stdout.readline().decode('utf-8').strip()
+    print('a:', after_merge_conflict_resolve_stdout)
+
+    assert before_merge_conflict_resolve_stdout == after_merge_conflict_resolve_stdout, \
+        f"Merge did not resolve to 'Abort' expected to see {before_merge_conflict_resolve_stdout}, " \
+        f"but instead got {after_merge_conflict_resolve_stdout}"
 
 
